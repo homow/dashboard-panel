357c41793c380e98c3271bdf09ccfa50f68ea3a2
homow <homowsh897@gmail.com>
2025-10-06 21:29:31 +0330

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import {defineConfig, loadEnv} from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from "@tailwindcss/vite";
+import path from 'path'
+import {fileURLToPath} from "url"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+export default defineConfig(({mode}) => {
+    const env = loadEnv(mode, process.cwd(), '')
+
+    return {
+        base: env.VITE_BASE_PATH || "/",
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'src'),
+            }
+        },
+        plugins: [
+            react(),
+            tailwindcss(),
+        ],
+    }
+})
