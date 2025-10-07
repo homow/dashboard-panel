6b9f652c3c47989c9566feceba4c2b3cc2ae5ef9
homow <homowsh897@gmail.com>
2025-10-07 19:42:35 +0330

    completed Account and ThemeScetion

--- src/layout/TopBar/index.jsx
+++ src/layout/TopBar/index.jsx
@@ -1,9 +1,13 @@
 import ThemeSection from './ThemeSection.jsx'
+import Account from "@/layout/TopBar/Account.jsx";
 
 export default function TopBar() {
     return (
-        <div>
-            <ThemeSection/>
+        <div className={"flex items-center justify-between"}>
+            <div className={"flex items-center justify-between gap-2"}>
+                <ThemeSection/>
+                <Account/>
+            </div>
         </div>
     )
 }
