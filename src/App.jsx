8f4e82c581bcc0f1649b7f42926da549eadaf819
homow <homowsh897@gmail.com>
2025-10-07 10:20:43 +0330

    config styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function App() {
     return (
-        <div>App</div>
+        <div>
+            <div className={"w-full py-5 bg-[#3A354161]"}>main</div>
+            <div className={"w-full py-5 bg-zinc-400"}>main</div>
+            <div className={"w-full py-5 bg-stone-800/70"}>main</div>
+            <div className={"w-full py-5 bg-gray-800"}>main</div>
+            <div className={"w-full py-5 bg-stone-700"}>main</div>
+        </div>
     )
 }
