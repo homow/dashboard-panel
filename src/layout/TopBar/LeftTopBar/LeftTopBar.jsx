2584a06baae86b35abce2cddb2cdb169b31c6d20
homow <homowsh897@gmail.com>
2025-10-11 11:23:12 +0330

    update SearchBar Header

--- src/layout/TopBar/LeftTopBar/LeftTopBar.jsx
+++ src/layout/TopBar/LeftTopBar/LeftTopBar.jsx
@@ -1,11 +1,9 @@
-import SearchBar from "./SearchBar.jsx";
 import OpenMobileNavMenuBtn from "./OpenMobileNavMenuBtn.jsx";
 
 export default function LeftTopBar({setMobileNavOpen}) {
     return (
-        <div>
-            <OpenMobileNavMenuBtn setMobileNavOpen={setMobileNavOpen} />
-            <SearchBar className={"hidden md:block"}/>
+        <div className={"md:hidden"}>
+            <OpenMobileNavMenuBtn setMobileNavOpen={setMobileNavOpen}/>
         </div>
     )
 }
--- src/layout/TopBar/index.jsx
+++ src/layout/TopBar/index.jsx
@@ -3,7 +3,7 @@ import LeftTopBar from "./LeftTopBar/LeftTopBar.jsx";
 
 export default function TopBar({setMobileNavOpen}) {
     return (
-        <header className={"flex items-center justify-between top-0 h-16"}>
+        <header className={"flex items-center justify-between top-0 h-16 md:justify-end"}>
 
             {/* left top bar */}
             <LeftTopBar setMobileNavOpen={setMobileNavOpen}/>
