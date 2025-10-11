bce509cb6bc967ce0578fa3c1f28d2d5ba133a1c
homow <homowsh897@gmail.com>
2025-10-11 20:19:57 +0330

    add chart to HomePage

--- src/pages/Home/HomePageLayout.jsx
+++ src/pages/Home/HomePageLayout.jsx
@@ -1,9 +1,11 @@
 import TopSection from "./TopSection/TopSection.jsx";
+import FirstChartHome from "./FirstChartHome/FirstChartHome.jsx";
 
 export default function HomePageLayout() {
     return (
-        <div className={"pt-12"}>
+        <div className={"pt-12 pb-5 space-y-6"}>
             <TopSection/>
+            <FirstChartHome/>
         </div>
     )
 }
