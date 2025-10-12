b55b874c231f14e0352c66814229749cd44205d5
homow <homowsh897@gmail.com>
2025-10-12 10:26:34 +0330

    completed DevMeetUp Component

--- src/pages/Home/BottomSection/DevMeetUp.jsx
+++ src/pages/Home/BottomSection/DevMeetUp.jsx
@@ -1,15 +1,32 @@
 import cn from "@/lib/utils/cn"
 import lapTopImg from "@img/home/Image.webp"
 
-const IconAction = ({icon, text, color = "text-secondary-txt"}) => {
+const DevMeetUpDate = ({text, subText, icon}) => {
     return (
-        <div>
+        <div className={"flex flex-row gap-2.5 items-start text-secondary-txt text-xs"}>
+            <span>
+                <svg className={"size-5"}>
+                    <use href={`#${icon}-icon`}></use>
+                </svg>
+            </span>
+
+            <div>
+                <p>{text}</p>
+                <p>{subText}</p>
+            </div>
+        </div>
+    )
+}
+
+const IconActionMeetUP = ({icon, text, color = "text-secondary-txt"}) => {
+    return (
+        <div className={"flex flex-col items-center justify-center gap-1"}>
             <span>
                 <svg className={cn(`size-5 ${color}`)}>
                     <use href={`#${icon}-icon`}></use>
                 </svg>
             </span>
-            <p className={"text-xs text-secondary-txt"}>{text}</p>
+            <p className={`text-xs ${color}`}>{text}</p>
         </div>
     )
 }
@@ -24,7 +41,7 @@ export default function DevMeetUp() {
             </div>
 
             {/* details wrapper */}
-            <div className={"px-5 pb-5"}>
+            <div className={"px-5 *:py-4 divide-y divide-divider-light"}>
                 {/* title and sub-title */}
                 <div className={"flex flex-row items-center gap-4"}>
                     {/* date wrapper */}
@@ -47,8 +64,17 @@ export default function DevMeetUp() {
                 </div>
 
                 {/* icon action wrapper */}
-                <div>
+                <div className={"flex flex-row items-center justify-between"}>
+                    <IconActionMeetUP icon={"circleTick"} text={"Interested"}/>
+                    <IconActionMeetUP icon={"user"} text={"Joined"} color={"text-sky-600"}/>
+                    <IconActionMeetUP icon={"star"} text={"Invited"}/>
+                    <IconActionMeetUP icon={"horizontalMore"} text={"More"}/>
+                </div>
 
+                {/* dev meetup date info */}
+                <div className={"space-y-2"}>
+                    <DevMeetUpDate text={"Tuesday, 24 january, 10:20 - 12:30"} icon={"clock"} subText={"After 1 week"}/>
+                    <DevMeetUpDate text={"The Richard NYC"} icon={"location"} subText={"1305 Lexington Ave, New York"}/>
                 </div>
             </div>
         </div>
