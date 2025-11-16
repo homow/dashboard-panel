dae2003e2d7f281ba1f5c2fc7d085a9a691e2226
homow <homowsh897@gmail.com>
2025-11-16 06:45:36 +0330

    refactor with AuthContext.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
-import AppRoutes from "@/routes/AppRoutes";
+import {Outlet} from "react-router-dom";
+import {AuthProvider, useAuth} from "@/context/AuthContext"
+import Loading from "@components/ui/Loading";
+
+function InnerApp() {
+    const {loading} = useAuth()
+
+    return (
+        loading ? (
+            <Loading className={"fixed inset-0 z-10"}/>
+        ) : (
+            <Outlet/>
+        )
+    )
+}
 
 export default function App() {
-    return <AppRoutes/>
-};
+    return (
+        <AuthProvider>
+            <InnerApp/>
+        </AuthProvider>
+    )
+}
