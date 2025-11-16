ef0c72d80bd1997483d57b3854d0723738039ebc
homow <homowsh897@gmail.com>
2025-11-16 07:12:13 +0330

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import {Outlet} from "react-router-dom";
 import {AuthProvider, useAuth} from "@/context/AuthContext"
 import Loading from "@components/ui/Loading";
+import AppRoutes from "@/routes/AppRoutes";
 
 function InnerApp() {
     const {loading} = useAuth()
@@ -9,7 +9,7 @@ function InnerApp() {
         loading ? (
             <Loading className={"fixed inset-0 z-10"}/>
         ) : (
-            <Outlet/>
+            <AppRoutes/>
         )
     )
 }
