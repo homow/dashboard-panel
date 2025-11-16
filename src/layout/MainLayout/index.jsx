b1dd9f44605a3762b290151b153c679963ce5dc1
homow <homowsh897@gmail.com>
2025-11-16 06:13:45 +0330

    refactor main layout

--- src/layout/MainLayout/index.jsx
+++ src/layout/MainLayout/index.jsx
@@ -1,11 +1,24 @@
-import AppRoutes from "@/routes/AppRoutes.jsx";
+import {MobileNavProvider, useMobileNav} from "@/context/MobileNavContext";
+import AppRoutes from "@/routes/AppRoutes";
+import Overlay from "@components/ui/Overlay";
 
-export default function MainLayout() {
+function InnerMainLayout() {
+    const {openMobileNav, setOpenMobileNav} = useMobileNav();
+
     return (
         <main id="main" className={"relative h-full pb-5 space-y-6 @container/main"}>
+            <Overlay/>
 
             {/* Routes */}
             <AppRoutes/>
         </main>
-    )
+    );
 }
+
+export default function MainLayout() {
+    return (
+        <MobileNavProvider>
+            <InnerMainLayout/>;
+        </MobileNavProvider>
+    );
+};
