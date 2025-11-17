ac2e2a5281faef75aa5ee51c5dbc9c9fe9a0763c
homow <homowsh897@gmail.com>
2025-11-18 02:14:45 +0330

    update UI

--- src/pages/Analytics/BottomSection/BottomSection.jsx
+++ src/pages/Analytics/BottomSection/BottomSection.jsx
@@ -6,7 +6,7 @@ const SalesOverviewChart = LazyWithSuspense(() => import("./SalesOverviewChart")
 
 export default function BottomSection() {
     return (
-        <section className={"grid grid-cols-1 gap-6 @2xl/main:grid-cols-2 @7xl/main:grid-cols-3"}>
+        <section className={"mt-6 grid grid-cols-1 gap-6 @2xl/main:grid-cols-2 @7xl/main:grid-cols-3"}>
             <TotalSalesChart className={"@2xl/main:col-span-2 @7xl/main:col-span-1 @7xl/main:h-auto"}/>
             <PerformanceChart className={""}/>
             <SalesOverviewChart className={""}/>
