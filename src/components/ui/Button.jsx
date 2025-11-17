e77df1e3b05e2d147e335420d78df44f601fdc9d
homow <homowsh897@gmail.com>
2025-11-18 01:52:51 +0330

    fix: Button padding

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -6,7 +6,7 @@ export default function Button({text = "Click", onClick, type = "button", classN
             type={type}
             onClick={onClick}
             className={cn(
-                "w-full bg-sky-600 hover:bg-sky-700 active:bg-sky-700 text-white py-2 rounded-lg transition font-medium cursor-pointer text-sm xs:text-base",
+                "w-full bg-sky-600 hover:bg-sky-700 active:bg-sky-700 text-white py-2 px-3 rounded-lg transition font-medium cursor-pointer text-sm xs:text-base",
                 disabled && "opacity-60 cursor-not-allowed",
                 className
             )}
